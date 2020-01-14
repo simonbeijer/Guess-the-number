97b2c5a10e3adddfb7cce27082949f1d17ef75e1
petelina17 <petelina17@gmail.com>
2020-01-14 19:45:02 +0100

    Change absolute path to relative path in scripts and img links

--- scripts/gamecontroller.js
+++ scripts/gamecontroller.js
@@ -27,13 +27,13 @@ class Gamecontroller {
         this.opponent1.addEventListener('click', () => {
             if (this.opponent1.classList.contains('not-selected1')) {
                 this.opponent1.style.border = '5px solid rgb(99,210,151)';
-                this.opponent1Img.setAttribute('src', '/img/op1selected.png');
+                this.opponent1Img.setAttribute('src','img/op1selected.png');
                 this.opponent1.classList.remove('not-selected1');
                 this.opponent2.style.border = 'none';
-                this.opponent2Img.setAttribute('src', '/img/op2.png');
+                this.opponent2Img.setAttribute('src','img/op2.png');
                 this.opponent2.classList.add('not-selected2');
                 this.opponent3.style.border = 'none';
-                this.opponent3Img.setAttribute('src', '/img/op3.png');
+                this.opponent3Img.setAttribute('src','img/op3.png');
                 this.opponent3.classList.add('not-selected3');
                 this.playBtn.style.opacity = '1';
             }
@@ -42,13 +42,13 @@ class Gamecontroller {
         this.opponent2.addEventListener('click', () => {
             if (this.opponent2.classList.contains('not-selected2')) {
                 this.opponent2.style.border = '5px solid rgb(99,210,151)';
-                this.opponent2Img.setAttribute('src', '/img/op2selected.png');
+                this.opponent2Img.setAttribute('src','img/op2selected.png');
                 this.opponent2.classList.remove('not-selected2');
                 this.opponent1.style.border = 'none';
-                this.opponent1Img.setAttribute('src', '/img/op1.png');
+                this.opponent1Img.setAttribute('src','img/op1.png');
                 this.opponent1.classList.add('not-selected1');
                 this.opponent3.style.border = 'none';
-                this.opponent3Img.setAttribute('src', '/img/op3.png');
+                this.opponent3Img.setAttribute('src','img/op3.png');
                 this.opponent3.classList.add('not-selected3');
                 this.playBtn.style.opacity = '1';
             }
@@ -57,13 +57,13 @@ class Gamecontroller {
         this.opponent3.addEventListener('click', () => {
             if (this.opponent3.classList.contains('not-selected3')) {
                 this.opponent3.style.border = '5px solid rgb(99,210,151)';
-                this.opponent3Img.setAttribute('src', '/img/op3selected.png');
+                this.opponent3Img.setAttribute('src','img/op3selected.png');
                 this.opponent3.classList.remove('not-selected3');
                 this.opponent2.style.border = 'none';
-                this.opponent2Img.setAttribute('src', '/img/op2.png');
+                this.opponent2Img.setAttribute('src','img/op2.png');
                 this.opponent2.classList.add('not-selected2');
                 this.opponent1.style.border = 'none';
-                this.opponent1Img.setAttribute('src', '/img/op1.png');
+                this.opponent1Img.setAttribute('src','img/op1.png');
                 this.opponent1.classList.add('not-selected1');
                 this.playBtn.style.opacity = '1';
             }
